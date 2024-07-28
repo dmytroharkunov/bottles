c1d176b2249efd635b603778270be80de6e837a4
Harkunov Dmytro <harkundmytro@gmail.com>
2024-07-28 13:13:15 +0300

    build process update

---
+++ src/app/components/config-modal/config-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-config-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './config-modal.component.html',
+  styleUrl: './config-modal.component.scss'
+})
+export class ConfigModalComponent {
+
+}
